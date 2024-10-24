d901d9b4144f2ebd0a9aed5b31205e58c2c580e5
Ilya <gashimovilya2018@gmail.com>
2024-10-24 23:53:35 +0300

    Upgrade 24/10

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,53 @@
-import reactImg from "./assets/react-core-concepts.png"
-
-import { CORE_CONCEPTS } from "./data"
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
-
-function genRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max + 1));
-}
-
-function CoreConcept({title, description, image, key}) {
-    return (
-        <li key={key}>
-            <img src={image} alt={title}/>
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </li>
-    )
-}
+import {CORE_CONCEPTS, EXAMPLES} from "./data"
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div>
-        <Header />
-      <main>
-          <section id="core-concepts">
-              <h2>Core Concepts</h2>
-              <ul>
-                  {CORE_CONCEPTS.map((item, index) => (
-                      <CoreConcept
-                          key={index}
-                          {...item}
-                      />
-                  ))}
-
-              </ul>
-          </section>
-
-      </main>
-    </div>
-  );
-}
-
-import React from 'react';
-
-function Header() {
-    const description = reactDescriptions[genRandomInt(2)];
+    const [tabContent, setTabContent] = useState('')
+    const handlerClick = (selectedButton) => {
+        console.log(selectedButton)
+        return setTabContent(selectedButton);
+    }
     return (
-        <header>
-            <img src={reactImg} alt="Stylized atom"/>
-            <h1>React Essentials</h1>
-            <p>
-                {description} React concepts you will need for almost any app you are
-                going to build!
-            </p>
-        </header>
+        <div>
+            <Header/>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        {CORE_CONCEPTS.map((item, index) => (
+                            <CoreConcept
+                                key={index}
+                                {...item}
+                            />
+                        ))}
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton isSelected={tabContent === 'components'} onSelect={() => handlerClick('components')}>Components</TabButton>
+                        <TabButton isSelected={tabContent === 'jsx'} onSelect={() => handlerClick('jsx')}>JSX</TabButton>
+                        <TabButton isSelected={tabContent === 'props'} onSelect={() => handlerClick('props')}>Props</TabButton>
+                        <TabButton isSelected={tabContent === 'state'} onSelect={() => handlerClick('state')}>State</TabButton>
+                    </menu>
+                    {!tabContent ? <p>Please select a topic.</p> :
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[tabContent].title}</h3>
+                            <p>{EXAMPLES[tabContent].description}</p>
+                            <pre>
+                                <code>
+                                    {EXAMPLES[tabContent].code}
+                                </code>
+                            </pre>
+                        </div>}
+                </section>
+            </main>
+        </div>
     );
 }
+
 
 export default App;
